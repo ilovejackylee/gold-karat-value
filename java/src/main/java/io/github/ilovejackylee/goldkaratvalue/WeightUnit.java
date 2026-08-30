4b3aa8a13c8d32e6540198489bd761622e5428af
ilovejackylee <ilovejackylee@proton.me>
2026-08-30 20:19:50 +0800

    Add java/src/main/java/io/github/ilovejackylee/goldkaratvalue/WeightUnit.java for JitPack

---
+++ java/src/main/java/io/github/ilovejackylee/goldkaratvalue/WeightUnit.java
@@ -0,0 +1,10 @@
+package io.github.ilovejackylee.goldkaratvalue;
+
+/** Weight unit accepted by the calculator. */
+public enum WeightUnit {
+    GRAM,
+    PENNYWEIGHT,
+    TROY_OUNCE,
+    TROY_POUND,
+    KILOGRAM
+}
