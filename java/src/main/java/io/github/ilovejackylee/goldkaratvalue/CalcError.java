f7ec792410eb757d5e9004e3f92347d3b389c7a9
ilovejackylee <ilovejackylee@proton.me>
2026-08-30 20:19:52 +0800

    Add java/src/main/java/io/github/ilovejackylee/goldkaratvalue/CalcError.java for JitPack

---
+++ java/src/main/java/io/github/ilovejackylee/goldkaratvalue/CalcError.java
@@ -0,0 +1,9 @@
+package io.github.ilovejackylee.goldkaratvalue;
+
+/** Why a calculation was rejected. */
+public enum CalcError {
+    NEGATIVE_WEIGHT,
+    INVALID_KARAT,
+    NEGATIVE_PRICE,
+    INVALID_DISCOUNT
+}
